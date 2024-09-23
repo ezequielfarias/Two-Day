884cb75266161b8cce8d2cc9b0288d4ca047a3cf
Ezequiel Adriel Leite  Farias <78233866+ezequielfarias@users.noreply.github.com>
2024-09-22 21:45:50 -0300

    arrumando tudo

--- js/dados.js
+++
@@ -1,9 +0,0 @@
-let dados = [
-  {
-    titulo: "Crackers",
-    descricao: "Indivíduos que utilizam suas habilidades técnicas para invadir sistemas de forma ilegal, com o objetivo de causar danos ou obter vantagens financeiras.",
-    link: "https://www.tecmundo.com.br/o-que-e/744-o-que-e-cracker-.htm"
-  },
-
-
-];
